176cfd1d7500344e7486d7a74bb7aa417861d617
suki2311 <sukichau0809@gmail.com>
2025-06-10 15:46:17 +0800

    Delete q2-js directory

--- q2-js/q2.js
+++
@@ -1,7 +0,0 @@
-settingsBtn.addEventListener('click', function() {
-    const languageItems = navmenu.querySelectorAll('.language-options');
-    languageItems.forEach(item => {
-        item.style.display = item.style.display === 'none' ? 'block' : 'none';
-    });
-    event.preventDefault();
-});
